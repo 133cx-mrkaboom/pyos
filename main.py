0673eb7e5f086a6c3de13183cc405c851ececdbb
Kaden Lee <mrkaboommc@gmail.com>
2025-06-27 16:48:32 +1000

    Made help file and main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+# PYOS
+# 2025 KADEN Lee
+# Took A lot Of Hard Work.
+# PLZ use
+# Thank You For Using
+
+import sys
+import modules.help
+
+print('''
+
+
+ ________  ___    ___ ________  ________      
+|\   __  \|\  \  /  /|\   __  \|\   ____\     
+\ \  \|\  \ \  \/  / | \  \|\  \ \  \___|_    
+ \ \   ____\ \    / / \ \  \\\  \ \_____  \   
+  \ \  \___|\/  /  /   \ \  \\\  \|____|\  \  
+   \ \__\ __/  / /      \ \_______\____\_\  \ 
+    \|__||\___/ /        \|_______|\_________\
+         \|___|/                  \|_________|
+                                              
+                                              
+2025 KADEN LEE
+
+
+Version ALPHA 0.0.1
+
+
+      ''')
+
+
+
+# exit commands
+
+def ext(message):
+    print("Shutdown Message: " + message) #shutdown message
+    print("Shuting Down") #shuting down
+    sys.exit() # exiting
+
+
+# Run Command
+
+def rn():
+    print("Running")
+
+    # code I need to put here after I make code
+    
+
---
+++ modules/help.py
@@ -0,0 +1,15 @@
+#help py
+
+def main():
+    print('''
+
+
+PYOS Help GUIDE
+
+exit - exits - useage: exit
+port - sets the port - useage: port <port>
+dir - sets the directory - useage: dir <directory>
+
+
+
+    ''')
